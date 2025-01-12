e0023cc8a31ab2da965968a6da6b2c2d9c0a68d4
FixxooXD <rajpalmal40@gmail.com>
2025-01-12 13:27:46 +0530

    Updated footer to support mobile view

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -4,20 +4,20 @@ const Footer = () => {
     return (
         <footer className="flex justify-center items-center bg-white py-1 border-t border-gray-200">
             <div className="container">
-                <div className="flex justify-between items-center space-y-4">
+                <div className="flex flex-col sm:flex-row justify-between items-center space-y-4">
                     <div className="flex items-center text-gray-700">
-                        <span>Built by Fixxoo with</span>
+                        <span className='sm:text-sm text-xs'>Built by Fixxoo with</span>
                         <Heart className="w-4 h-4 mx-1 text-red-500 fill-current animate-pulse" />
                         <span>love</span>
                     </div>
-                    <div className="flex space-x-6">
+                    <div className="flex space-x-4 sm:space-x-6">
                         <a
                             href="https://x.com/Raj_FixxooXD"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-gray-600 hover:text-orange-500 transition-colors duration-300"
                         >
-                            <Twitter className="w-6 h-6" />
+                            <Twitter className="w-5 h-5 sm:w-6 sm:h-6" />
                         </a>
                         <a
                             href="https://www.linkedin.com/in/raj-palmal-a736ab233/"
@@ -25,7 +25,7 @@ const Footer = () => {
                             rel="noopener noreferrer"
                             className="text-gray-600 hover:text-orange-500 transition-colors duration-300"
                         >
-                            <Linkedin className="w-6 h-6" />
+                            <Linkedin className="w-5 h-5 sm:w-6 sm:h-6" />
                         </a>
                         <a
                             href="https://github.com/FixxooXD"
@@ -33,7 +33,7 @@ const Footer = () => {
                             rel="noopener noreferrer"
                             className="text-gray-600 hover:text-orange-500 transition-colors duration-300"
                         >
-                            <Github className="w-6 h-6" />
+                            <Github className="w-5 h-5 sm:w-6 sm:h-6" />
                         </a>
                     </div>
 
